4c2dc6e4f0f69222f8cb92647c6f255baa43a80b
Lucyna Błaszczyk <lucyna.blaszczyk@altkomsoftware.pl>
2022-03-03 15:05:47 +0100

    add navigation

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,13 +1,23 @@
 import {startMarketing} from 'marketing/MarketingApp';
 import React, {useRef, useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
 
 export default () => {
     const ref = useRef(null);
+    const history = useHistory();
 
     useEffect(() => {
-        startMarketing(ref.current);
-        
-    });
+        const {onParentNavigate} = startMarketing(ref.current, {
+            onNavigate: ({pathname: nextPathname}) => {
+                const {pathname} = history.location;
+
+                if (pathname !== nextPathname) {
+                    history.push(nextPathname);
+                }
+            }
+        });
+        history.listen(onParentNavigate);
+    }, []);
 
     return <div ref={ref}/>
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createMemoryHistory, createBrowserHistory} from 'history';
 import App from './App';
 
 //Function to start up the app
-const startMarketing = (el) => {
-    ReactDOM.render(<App />, el);
+const startMarketing = (el, {onNavigate, defaultHistory}) => {
+    const history = defaultHistory || createMemoryHistory();
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
+    ReactDOM.render(<App history={history}/>, el);
+
+    return {
+        onParentNavigate({pathname: nextPathname}) {
+            const {pathname} = history.location;
+
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    };
 };
 
 //If we are in development in isolation
 //call start function immediately
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
-    if(devRoot){
-        startMarketing(devRoot);
+    if (devRoot) {
+        startMarketing(devRoot, {defaultHistory: createBrowserHistory()});
     }
 }
 
