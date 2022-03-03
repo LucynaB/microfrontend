a8ab532dc15d0824bbb658ee1925e38e46332923
Lucyna Błaszczyk <lucyna.blaszczyk@altkomsoftware.pl>
2022-03-03 13:32:57 +0100

    add generateClassName

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
+import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
+//to prevent css class name collision
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+});
 
 export default () => {
     return (
         <BrowserRouter>
             <div>
-                <Header/>
-                <MarketingApp/>
+                <StylesProvider generateClassName={generateClassName}>
+                    <Header/>
+                    <MarketingApp/>
+                </StylesProvider>
             </div>
         </BrowserRouter>
     );
