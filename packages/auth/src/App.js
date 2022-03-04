5baa64c5bf16d12f8dfea965d183587d337d17f9
Lucyna Błaszczyk <lucyna.blaszczyk@altkomsoftware.pl>
2022-03-04 12:19:51 +0100

    add auth logic

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -5,20 +5,23 @@ import Signin from './components/Signin';
 import Signup from './components/Signup';
 
 
-
 //to prevent css class name collision
 const generateClassName = createGenerateClassName({
     productionPrefix: 'au'
 });
 
-export default ({history}) => {
+export default ({history, onSignIn}) => {
     return (
         <div>
             <StylesProvider generateClassName={generateClassName}>
                 <Router history={history}>
                     <Switch>
-                        <Route exact path='/auth/signin' component={Signin}/>
-                        <Route path='/auth/signup' component={Signup}/>
+                        <Route exact path='/auth/signin'>
+                            <Signin onSignIn={onSignIn} />
+                        </Route>
+                        <Route path='/auth/signup'>
+                            <Signup onSignIn={onSignIn} />
+                        </Route>
                     </Switch>
                 </Router>
             </StylesProvider>
